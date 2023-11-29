60678c3ea536fe73fe0406872a3fd07853e57aa6
khabibegle16 <khabibegle16@gmail.com>
2023-11-29 08:12:28 +0530

    29-11

--- server/v1/index.js
+++ server/v1/index.js
@@ -1,6 +1,7 @@
 const app = require('express')
 const v1_router = app.Router()
-
+const {apiMiddleware1} = require("./modules/module")
+v1_router.use(apiMiddleware1)
 v1_router.all("/",(req,res)=>{
     res.send("api v1")
 })
--- server/v1/modules/module.js
+++ server/v1/modules/module.js
@@ -1,7 +1,7 @@
 // const { query } = require("express-validator");
 // const {addNewRecordIdempotancy} = require("./modules")
 // import {addNewRecordIdempotancy} from "./modules.js"
-// var idempotency_local_storage = [];
+var idempotency_local_storage = [];
 
 // app.get("/idempotancy_check", (req, res) => {
 //     // console.log("called",path.join(__dirname,"config.txt"),fs.existsSync(path.join(__dirname,"client","config.txt")))
@@ -11,22 +11,24 @@
 //       res.status(300).send("File Does not exit");
 //     }
 //   });
-// const apiMiddleware1 = (req, res, next) => {
-//   // your-middleware-logic
-//   // console.log(req);
+const apiMiddleware1 = (req, res, next) => {
+  // your-middleware-logic
+  // console.log(req);
 
-//   if (req.headers["idempotency-key"]) {
-//     console.log(idempotency_local_storage.indexOf(req.headers["idempotency-key"]))
-//     if (idempotency_local_storage.indexOf(req.headers["idempotency-key"])!==-1) {
-//       res.status(200).send("request already fulfilled!");
-//     } else {
+  if (req.headers["idempotency-key"]) {
+   // console.log(idempotency_local_storage.indexOf(req.headers["idempotency-key"]))
+    if (idempotency_local_storage.indexOf(req.headers["idempotency-key"])!==-1) {
+      res.status(200).send("request already fulfilled!");
+    } else {
 
-//       idempotency_local_storage.push(req.headers["idempotency-key"])
-//       next();
-//     }
-//   } else {
-//     res.status(300).send("idempotency key is missing");
-//   }
-// };
+      idempotency_local_storage.push(req.headers["idempotency-key"])
+      next();
+    }
+  } else {
+    res.status(300).send("idempotency key is missing");
+  }
+};
+
+module.exports ={apiMiddleware1}
 // app.use(apiMiddleware1);
 // app.use(apiMiddleware1);
